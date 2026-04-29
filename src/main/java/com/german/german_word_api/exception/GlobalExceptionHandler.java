693b80e2bece053c3aa98efb2b596904f430f828
Özge Atik <ozge@Ozge-MacBook-Air.local>
2026-04-29 21:42:19 +0300

    exception handler added

---
+++ src/main/java/com/german/german_word_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.german.german_word_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex){
+
+        Map<String,String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return errors;
+    }
+}
